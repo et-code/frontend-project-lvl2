f96e709586cb1a87f21634370a6c21d53f63fb08
Evgeniy Trifonov <4etrifonov@gmail.com>
2022-03-03 17:47:18 +0300

    Add arguments & option -f into gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
   .version('0.0.0', '-V, --version', 'output the version number')
   .description('Compares two configuration files and show difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
